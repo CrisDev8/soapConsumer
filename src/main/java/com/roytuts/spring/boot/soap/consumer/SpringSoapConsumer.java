be65226898934fc0233bb1fbc94ea64d10b9e78a
cristiansilva <cristiansilva@cbit-online.com>
2020-03-13 10:27:14 -0500

    Css- emision inicial

---
+++ src/main/java/com/roytuts/spring/boot/soap/consumer/SpringSoapConsumer.java
@@ -0,0 +1,34 @@
+package com.roytuts.spring.boot.soap.consumer;
+
+
+public class SpringSoapConsumer {
+
+	public static void main(String[] args) {
+		String ciudades= "";
+
+		System.out.println("LAS CIUDADES SON:"+ ciudades);
+//		UserPort userPort = new UserPortService().getUserPortSoap11();
+//		GetUserDetailsRequest request = new GetUserDetailsRequest();
+//		request.setName("souvik");
+//		GetUserDetailsResponse response = userPort.getUserDetails(request);
+//		for (User user : response.getUsers()) {
+//			System.out.println(user.getId() + ", " + user.getName() + ", " + user.getEmail() + ", ["
+//					+ user.getAddress().getStreet() + ", " + user.getAddress().getCity() + ", "
+//					+ user.getAddress().getState() + ", " + user.getAddress().getZip() + ", "
+//					+ user.getAddress().getCountry() + ", " + user.getAddress().getAddressType() + "]");
+//		}
+//
+//		System.out.println(
+//				"------------------------------------------------------------------------------------------------");
+//
+//		request.setName("l");
+//		response = userPort.getUserDetails(request);
+//		for (User user : response.getUsers()) {
+//			System.out.println(user.getId() + ", " + user.getName() + ", " + user.getEmail() + ", ["
+//					+ user.getAddress().getStreet() + ", " + user.getAddress().getCity() + ", "
+//					+ user.getAddress().getState() + ", " + user.getAddress().getZip() + ", "
+//					+ user.getAddress().getCountry() + ", " + user.getAddress().getAddressType() + "]");
+//		}
+	}
+
+}
